d1a607e2f77a682915f2f69306a18694cb4dcb58
joeygracesabusido <joeysabusido@HFC006.duraville.org>
2024-01-20 08:16:33 +0800

    01.20.2024

---
+++ main.py
@@ -0,0 +1,34 @@
+
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+from routes.admin import admin
+#from routes.forms import form_htlm
+#from routes.graph import  graphql_app
+
+
+app = FastAPI()
+
+
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['*'],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+app.include_router(admin)
+#app.include_router(form_htlm)
+#app.include_router(graphql_app, prefix="/graphql")
+# app.include_router(graph)
+
+# app.include_router(graphql_app, prefix="/graphql")
