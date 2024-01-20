5c79f449d04da262386989310e785f323172aa23
joeygracesabusido <joeygracesabusido@gmail.com>
2024-01-20 16:47:01 +0800

    testing2

--- routes/admin.py
+++ routes/admin.py
@@ -8,7 +8,7 @@ from typing import Union, List, Optional
 from datetime import datetime, timedelta
 
 
-from  ..database.mongodb import create_mongo_client
+from  routes.database.mongodb_connection import create_mongo_client
 mydb = create_mongo_client()
 
 
